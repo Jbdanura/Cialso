5a94a72cec94184940b2636303fe52535bc6662d
unknown <danurabe@gmail.com>
2022-11-20 18:41:16 -0300

    post creation

--- back/controllers/posts.js
+++ back/controllers/posts.js
@@ -1,5 +1,9 @@
+require("dotenv").config({path:"./keys/.env"})
+
 const Post = require("../models/Post")
+const User = require("../models/User").User
 const postsRouter = require("express").Router()
+const jwt = require("jsonwebtoken")
 
 const getToken = req => {
     const authorization = req.get("authorization")
@@ -12,9 +16,15 @@ const getToken = req => {
 postsRouter.post("/new", async(req,res)=>{
     const description = req.body.description
     const token = getToken(req)
-    console.log(description)
-    console.log(token)
-    
+    const decodedToken = jwt.verify(token,process.env.SECRET)
+    if(!decodedToken.id){
+        return response.status(401).json({error: "token missing or invalid"})
+    }
+    const user = await User.findByPk(decodedToken.id)
+    const post = await Post.create({
+        description,
+        UserId: decodedToken.id
+    })
     res.json(description)
 })
 
