db6bc2a1159903d464fb124b68146b159e4b92d7
unknown <danurabe@gmail.com>
2022-12-04 00:04:08 -0300

    post user display

--- front/src/components/User.js
+++ front/src/components/User.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import Post from './Post'
 
 const User = ({baseUrl,user}) => {
   const username = useParams().username
@@ -15,14 +16,13 @@ const User = ({baseUrl,user}) => {
     }
   ,[username])
 
-
   if (!user){
     return null
   } 
 
   return (
     <>
-    {userData && userData.username == username ? 
+    {userData && 
       <div className="user-container">
         <div className="user-data">
           {userData.avatar ? <img className="avatar" src={`${baseUrl + userData.username}.png`}/> : 
@@ -30,26 +30,26 @@ const User = ({baseUrl,user}) => {
           <div className="info">
             <p className="name">{userData.name} {userData.lastname}</p>
             <p className="username">@{userData.username}</p>
+            <div className="follow-container">
+              <p className="followers"><span style={{fontWeight:"bolder"}}>10</span> followers</p>
+              <p className="following"><span style={{fontWeight:"bolder"}}>13</span> following</p>
+              {userData.username != user.username &&
+                <p className="follow">Follow</p>
+              }
           </div>
+          </div>
+
         </div> 
         {userData.Posts.length > 0 ? 
           <div className="user-posts">
-            {userData.Posts.map(post=>{
+            {userData.Posts.slice(0).reverse().map(post=>{
               /*{id: 10, description: 'hola', createdAt: '2022-11-20T21:40:38.997Z',
               updatedAt: '2022-11-20T21:40:38.997Z', UserId: 6}UserId: 6createdAt: "2022-11-20T21:40:38.997Z"description:
                "hola"id: 10updatedAt: "2022-11-20T21:40:38.997Z"[[Prototype]]: Object*/
-              return <div className="user-post" key={post.id}>
-                <div className="post-info">
-                  {userData.avatar ? <img className="avatar" src={`${baseUrl + userData.username}.png`}/> : 
-                  <img className="post-avatar" src={"/user.png"}/>}
-                  <p className="post-name">{userData.name} {userData.lastname} 
-                  <span className="post-username">@{userData.username}</span></p>
-                </div>
-                <p className="post-text">{post.description}</p>
-              </div>
+              return <Post key={post.id} post={post} userData={userData} baseUrl={baseUrl} user={user}></Post>
             })}
           </div> : null}
-        </div>: null}
+        </div>}
     </>
 
   )
